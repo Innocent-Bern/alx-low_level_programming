baeec98ba57cb054a2a5c72fe51d236424674a68
Innocent_Bern <innocentbern22@gmail.com>
2023-02-16 20:51:15 +0300

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - C program that prints a using the printf function
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
