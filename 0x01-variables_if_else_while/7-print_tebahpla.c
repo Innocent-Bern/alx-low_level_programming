18f846db72abd76d77dc57803e2f2f5af5d18341
Innocent_Bern <innocentbern22@gmail.com>
2023-02-17 22:20:38 +0300

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - Entry point
+* Description: prints the alphabet in lowercase and in reverse
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
