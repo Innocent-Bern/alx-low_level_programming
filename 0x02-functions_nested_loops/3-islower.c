904bb38486f3d7379cba9e4f0f7cd8a9c774766f
Innocent_Bern <innocentbern22@gmail.com>
2023-02-22 01:15:52 +0300

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,10 +6,11 @@
 */
 int _islower(int c)
 {
-    if (c >='A' && c <= 'Z')
+    if (c >= 'A' && c <= 'Z')
     {
         return (0);
-    }else if (c >= 'a' && c <= 'z')
+    }
+    else if (c >= 'a' && c <= 'z')
     {
         return (1);
     }
