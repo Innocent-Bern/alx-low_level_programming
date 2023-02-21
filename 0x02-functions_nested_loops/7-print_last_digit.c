5618da1f14551614c2a0b1a46759d764a52f241f
Innocent_Bern <innocentbern22@gmail.com>
2023-02-22 02:09:53 +0300

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * Description: returns the last value of an interger
+ * Return: int
+ */
+
+int print_last_digit(int n)
+{
+    return ( n % 10 );
+}
