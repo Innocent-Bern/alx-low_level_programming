bc591cdad74535f9936ac2f854a6d5565b1cc517
Innocent_Bern <innocentbern22@gmail.com>
2023-02-21 19:03:17 +0300

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description: prints _putchar with the putchar() funtion
+* Return: Always 0 (success)
+*/
+
+int main()
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	return (0);
+}
