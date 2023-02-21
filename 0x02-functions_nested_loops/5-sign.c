ef788af8a49fe9c97463f54c44a37dd371f4397b
Innocent_Bern <innocentbern22@gmail.com>
2023-02-22 01:44:42 +0300

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_sign - Entry point
+* Description: checks if n greater than or equal to zero
+* @n: the integer value it receives
+* Return: 1 if true. 0 if false.
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+    _putchar(+);
+    return (1);
+}
+else if (n == 0)
+{
+    _putchar(0);
+    return (0);
+}
+else if (n < 0)
+{
+    _putchar(-1);
+    return (-1);
+}
+return (0);
+}
