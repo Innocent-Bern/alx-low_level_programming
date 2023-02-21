e451e815ceb1f327bf10736ce457fb91d02e0103
Innocent_Bern <innocentbern22@gmail.com>
2023-02-22 00:28:08 +0300

    task 1 changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,16 @@
 * Description: prints the alphabet without using the standard library
 * Return: Always 0 (success)
 */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 void print_alphabet(void)
 {
 	char letter;
-	
+
 	for(letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
