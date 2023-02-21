6661d1f8c8d12976e66067bfbca41f675966de3e
Innocent_Bern <innocentbern22@gmail.com>
2023-02-22 00:34:15 +0300

    task 1 changes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,10 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
 * print_alphabet - Entry point
 * Description: prints the alphabet without using the standard library
 * Return: Always 0 (success)
 */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 void print_alphabet(void)
 {
