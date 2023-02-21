28ba1325a196e4a355ddb3b95328c9791ad7352d
Innocent_Bern <innocentbern22@gmail.com>
2023-02-22 02:52:45 +0300

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* jack_bauer - Entry point
+* Description: prints every minute of the day
+* Return: void
+*/
+
+void jack_bauer(void)
+{
+    int minute, hours;
+
+    for (hours = 0; hours < 24; hours++)
+    {
+        for (minute =0; minute < 60; minute++)
+        {
+            if (hours < 10){
+                _putchar('0');
+                _putchar(hours + '0');
+                _putchar(':');
+            }
+            else if (hours > 10)
+            {
+                _putchar(hours + '0');
+                _putchar(':');
+            }
+            if (minute < 10)
+            {
+                _putchar('0');
+                _putchar(minute + '0');
+            }
+            else if (minute > 10)
+            {
+                _putchar(minute + '0');
+            }
+            _putchar('\n');
+        }
+    }
+    return (0);
+}
