5bcd92e5adfc387625ba7dea865c6697c8b895c0
Innocent_Bern <innocentbern22@gmail.com>
2023-02-22 05:03:24 +0300

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_to_98 - Entry point
+* @n: starting interger
+* Description: adds two integers and returns the result
+* Return: void
+*/
+void print_to_98(int n)
+{
+    while(n >= 98)
+    {
+        if(n == 98)
+        {
+            printf('98')
+        }
+        else
+        {
+        printf('%d, ', n);
+        n--;
+        }
+    }
+    while(n <= 98)
+    {
+        if(n == 98)
+        {
+            printf('98')
+        }
+        else
+        {
+            printf('%d, ', n);
+            n++;
+        }
+    }
+}
