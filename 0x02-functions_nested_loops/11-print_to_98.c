bf9986d37250a76367bbb4ba074159175263d99f
Innocent_Bern <innocentbern22@gmail.com>
2023-02-22 05:10:12 +0300

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,11 +13,11 @@ void print_to_98(int n)
     {
         if(n == 98)
         {
-            printf('%d', n);
+            printf("%d", n);
         }
         else
         {
-        printf('%d, ', n);
+        printf("%d, ", n);
         n--;
         }
     }
@@ -25,11 +25,11 @@ void print_to_98(int n)
     {
         if(n == 98)
         {
-            printf('%d', n);
+            printf("%d", n);
         }
         else
         {
-            printf('%d, ', n);
+            printf("%d, ", n);
             n++;
         }
     }
