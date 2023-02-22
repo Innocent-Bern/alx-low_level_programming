591944b82384c6bd66da1d74f005f7bea0455642
Innocent_Bern <innocentbern22@gmail.com>
2023-02-22 04:38:58 +0300

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* add - Entry point
+* Description: adds two integers and returns the result
+* Return: int
+*/
+int add(int j, int n)
+{
+    _putchar((j+n) +'0');
+	return (j + n);
+}
