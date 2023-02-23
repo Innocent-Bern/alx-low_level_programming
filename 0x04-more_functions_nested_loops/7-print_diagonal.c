893c09b776a32a7eb75eddcb033a26fa2759d380
Innocent_Bern <innocentbern22@gmail.com>
2023-02-23 20:35:18 +0300

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_diagonal - Entry point
+* Description: prints diagonal line of length n
+* @n: integer value
+* Return: void
+*/
+void print_diagonal(int n)
+{
+	int i = 0, ii;
+
+	while (i < n && n > 0)
+	{
+		ii = 0;
+		while (ii < i)
+		{
+			_putchar(' ');
+			ii++;
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
