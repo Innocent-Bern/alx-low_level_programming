d8147399810165506b2f2a5bd65e53e137a3f99f
Innocent_Bern <innocentbern22@gmail.com>
2023-02-23 19:14:16 +0300

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isupper - Entry point
+* Description: checks if int c letter is lowercase
+* @c: the integer value it receives
+* Return: 1 if true. 0 if false.
+*/
+int _islower(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+    return (1);
+    }
+return (0);
+}
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,41 +7,23 @@
 * Return: void
 */
 
-// void print_square(int size)
-// {
-//     int i;
-//     int y;
-
-//     if (size <= 0)
-//     {
-//         _putchar('\n');
-//         return;
-//     }
-
-//     for (i = 0; i <= size; i++)
-//     {
-//         for (y = 0; y <= size; y++)
-//         {
-//             _putchar('#');
-//         }
-//         _putchar('\n');
-//     }
-// }
-void print_square(int n)
+void print_square(int size)
 {
-	int i = 0, ii;
+    int i;
+    int y;
 
-	while (i < n && n > 0)
-	{
-		ii = 0;
-		while (ii < n)
-		{
-			_putchar('#');
-			ii++;
-		}
-		_putchar('\n');
-		i++;
-	}
-	if (i == 0)
-		_putchar('\n');
+    if (size <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    for (i = 0; i <= size; i++)
+    {
+        for (y = 0; y <= size; y++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
 }
