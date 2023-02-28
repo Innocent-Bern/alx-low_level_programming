8b5e8aab44abd0f88a9fb792355f5414c6abefb1
Innocent_Bern <innocentbern22@gmail.com>
2023-02-28 14:20:04 +0300

    auto readme

--- tests/comments.c
+++ tests/comments.c
@@ -1,4 +1,5 @@
-# comments.c
+#include "holberton.h"
+
 /**
  * _strncat - Concatenates two strings using at most
  *            an inputted number of bytes from src.
@@ -8,3 +9,13 @@
  *
  * Return: A pointer to the resulting string dest.
  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
