ae7a8262ac014daafcd99688aa00c3d25623ab2a
Innocent_Bern <innocentbern22@gmail.com>
2023-02-28 05:14:05 +0300

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_arrays.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_array - Entry point
+* Description: Prints n elements of an array of intergers
+* @a: interger input
+* @n: interger input
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; n--, i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
