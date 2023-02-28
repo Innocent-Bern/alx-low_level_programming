e024b7013b466304621e747163ea15b9978a6434
Innocent_Bern <innocentbern22@gmail.com>
2023-02-28 04:40:51 +0300

    task 5

--- 0x05-pointers_arrays_strings/test.c
+++ 0x05-pointers_arrays_strings/test.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+void print_rev(char *s)
+{
+    int i = 0;
+
+    while (s[i])
+    {
+        i++;
+    }
+    while (i--)
+    {
+        _putchar(s[i]);
+    }
+    _putchar('\n');
+}
 
 
 int main(void)
 {
-    _strlen("Yes");
+    print_rev("Yes");
     return (0);
 }
