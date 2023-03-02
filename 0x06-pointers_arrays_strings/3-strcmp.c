5a4887149929a0a3b6cf7eb8aa13a79431a7f3b5
Innocent_Bern <innocentbern22@gmail.com>
2023-03-03 01:37:12 +0300

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,6 +10,20 @@
 int _strcmp(char *s1, char *s2)
 {
     int check = 0;
+    int len1, len2;
+
+    len1 = 0;
+    len2 = 0;
+
+    while (s1[len1])
+        len1++;
+    while (s2[len2])
+        len2++;
+
+    if (len1 != len2)
+    {
+        return (1);
+    }
 
     while (s1[check] && s2[check])
     {
--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-
-char *_strcat(char *dest, char *src)
+int _strcmp(char *s1, char *s2)
 {
-    
+    int check = 0;
+    int len1, len2;
+
+    len1 = 0;
+    len2 = 0;
+
+    while (s1[len1])
+        len1++;
+    while (s2[len2])
+        len2++;
+
+    if (len1 != len2)
+    {
+        return (1);
+    }
+    while (s1[check] && s2[check])
+    {
+        if (s1[check] == s2[check])
+        {
+            continue;
+        }else
+        {
+            return (1);
+        }
+        check++;
+    }
+    return (0);
 }
 
 int main(void)
@@ -12,15 +37,7 @@ int main(void)
     char *destpt = &dest[0];
     char str[] = "Hello";
     char *strpt = &str[0];
-    int strlen, destlen, i;
 
-    i = 1;
-
-    for (strlen = 0; strlen <= i; strlen++)
-    {
-        *(destpt + strlen) = strpt[strlen];
-    }
-    *(destpt + (i)) = '\0';
-    printf("%s\n", destpt);
+    _strcmp(destpt, strpt);
     return (0);
 }
