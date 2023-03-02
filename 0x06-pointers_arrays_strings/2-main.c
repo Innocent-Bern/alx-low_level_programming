6b3c094f15547673cad058353c815a7a770708bf
Innocent_Bern <innocentbern22@gmail.com>
2023-03-02 21:58:58 +0300

    task 2

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -6,32 +6,40 @@
  *
  * Return: Always 0.
  */
- char *_strncpy(char *dest, char *src, int n)
-{
-    int destlen, srclen;
 
-    destlen = 0;
-    srclen = 0;
+/**
+* _strncpy : Function that copies string src up to n to the dest string
+* @dest: input string
+* @src: input string
+* @n: input inerger
+* Return : pointer to the resulting string dest
+*/
 
-    for (srclen = 0; src[srclen] && srclen < n; srclen++)
-    {
-        dest[srclen] = src[srclen];
-    }
-    *(dest + (n)) = '\0';
-    return (dest);
+char *_strncpy(char *dest, char *src, int n)
+{ 
+    // int destlen, srclen;
 
-    // int index = 0, src_len = 0;
+    // destlen = 0;
+    // srclen = 0;
 
-	// while (src[index++])
-	// 	src_len++;
+    // for (srclen = 0; src[srclen] && srclen < n; srclen++)
+    // {
+    //     dest[srclen] = src[srclen];
+    // }
+    // *(dest + (n+1)) = '\0';
+    // return (dest);
+    int index = 0, src_len = 0;
 
-	// for (index = 0; src[index] && index < n; index++)
-	// 	dest[index] = src[index];
+	while (src[index++])
+		src_len++;
 
-	// for (index = src_len; index < n; index++)
-	// 	dest[index] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
 
-	// return (dest);
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
 }
 
 int main(void)
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * _strncpy : Function that copies string src up to n to the dest string
 * @dest: input string
@@ -9,19 +8,8 @@
 */
 
 char *_strncpy(char *dest, char *src, int n)
-{ 
-    // int destlen, srclen;
-
-    // destlen = 0;
-    // srclen = 0;
-
-    // for (srclen = 0; src[srclen] && srclen < n; srclen++)
-    // {
-    //     dest[srclen] = src[srclen];
-    // }
-    // *(dest + (n+1)) = '\0';
-    // return (dest);
-    int index = 0, src_len = 0;
+{
+	int index = 0, src_len = 0;
 
 	while (src[index++])
 		src_len++;
