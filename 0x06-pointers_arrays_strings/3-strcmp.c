2533c60a060e988ed9bf56b4a448b5f845d0c96f
Innocent_Bern <innocentbern22@gmail.com>
2023-03-03 01:28:36 +0300

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strcmp : Function that compares two strings
+* @s2: input string
+* @s1: input string
+* Return : an int 1 or 0
+*/
+
+int _strcmp(char *s1, char *s2);
+{
+    int check = 0;
+
+    while (s1[check] && s2[check])
+    {
+        if (s1[check] == s2[check])
+        {
+            continue
+        }else
+        {
+            return (1);
+        }
+        check++;
+    }
+    return (0);
+}
