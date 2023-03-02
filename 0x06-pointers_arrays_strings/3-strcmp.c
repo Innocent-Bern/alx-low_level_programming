4a6cae078811f3ecdabf422d06a662757be312bc
Innocent_Bern <innocentbern22@gmail.com>
2023-03-03 01:41:52 +0300

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,39 +9,32 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    // int check = 0;
-    // int len1, len2;
+    int check = 0;
+    int len1, len2;
 
-    // len1 = 0;
-    // len2 = 0;
+    len1 = 0;
+    len2 = 0;
 
-    // while (s1[len1])
-    //     len1++;
-    // while (s2[len2])
-    //     len2++;
+    while (s1[len1])
+        len1++;
+    while (s2[len2])
+        len2++;
 
-    // if (len1 != len2)
-    // {
-    //     return (1);
-    // }
+    if (len1 != len2)
+    {
+        return (1);
+    }
 
-    // while (s1[check] && s2[check])
-    // {
-    //     if (s1[check] == s2[check])
-    //     {
-    //         continue;
-    //     }else
-    //     {
-    //         return (1);
-    //     }
-    //     check++;
-    // }
-    // return (0);
-	while ((*s1 && *s2) && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
-
-	return (*s1 - *s2);    
+    while (s1[check] && s2[check])
+    {
+        if (s1[check] == s2[check])
+        {
+            continue;
+        }else
+        {
+            return (1);
+        }
+        check++;
+    }
+    return (0);
 }
