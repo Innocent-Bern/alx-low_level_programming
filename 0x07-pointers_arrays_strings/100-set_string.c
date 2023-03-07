4342d61c8adf6e5652fdb0eb146dfc8e8aa38085
Innocent_Bern <innocentbern22@gmail.com>
2023-03-07 22:04:53 +0300

    task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _print_chessboard : Function that sets the value of a pointer to a char.
+* @s: string input
+* @to: string input
+* Return : void
+*/
+
+void set_string(char **s, char *to)
+{
+    *s = to;
+}
