59f48118a2880c7fdeb183d0eeabf6e147cf9a3e
Innocent_Bern <innocentbern22@gmail.com>
2023-03-10 21:45:02 +0300

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,17 +11,6 @@
 
 int main( int argc, char *argv[])
 {
-	int i;
-
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
-
-	return (0);
+    printf("%d\n", argc - 1);
+    return (0);
 }
