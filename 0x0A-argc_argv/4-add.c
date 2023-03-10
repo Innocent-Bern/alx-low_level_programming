6aeaa3a7d482707aa9a60c496f1ff2c9b4e49d84
Innocent_Bern <innocentbern22@gmail.com>
2023-03-11 00:26:33 +0300

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,7 +16,11 @@ int main( int argc, char *argv[])
     int i, mysum;
 
     mysum = 0;
-    
+    if (argc == 1)
+    {
+        printf("0\n");
+        return (0);
+    }
     for (i = 1; i < argc; i++)
     {
         if (atoi(argv[i]) == 0 && *argv[i] != '0')
