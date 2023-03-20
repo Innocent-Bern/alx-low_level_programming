96347c760a7fc720021f64efd89fdfb04136e7a4
Innocent_Bern <innocentbern22@gmail.com>
2023-03-20 18:08:43 +0300

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,13 @@
-# 0x0D-preprocessor
-## 2-main.c
+#include <stdio.h>
+
+/**
 * main: Entry point
 * Description: A program that prints the name of the file it was compiled fron followed by a newline
 * Return: 0
+*/
+
+int main(void)
+{
+    printf("%s\n", __FILE__);
+    return (0);
+}
