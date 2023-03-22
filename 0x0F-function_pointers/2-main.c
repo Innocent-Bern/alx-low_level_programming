8108897deff4c01d3a55189d740df6e5668937a0
Innocent_Bern <innocentbern22@gmail.com>
2023-03-22 23:25:08 +0300

    task 1 changes

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -20,22 +20,18 @@ int is_98(int elem)
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i, result;
-
-    if (size <= 0)
-        return -1;
-
-    for (i = 0; i < size; i++)
-    {
-        result = cmp(array[i]);
-        if (result != 0)
-        {
-            result = i;
-            break;
-        }
-    }
-    return (result);
+int i, result;
+if (size <= 0)
+	return (-1);
+for (i = 0; i < size; i++)
+{
+result = cmp(array[i]);
+if (result != 0)
+return (i);
 }
+return (-1);
+}
+
 
 int is_strictly_positive(int elem)
 {
