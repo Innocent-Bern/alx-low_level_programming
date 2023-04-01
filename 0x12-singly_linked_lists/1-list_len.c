c6330a0b57ddcb950a65a225ddc79a06b5c16bd6
Innocent_Bern <innocentbern22@gmail.com>
2023-04-01 04:27:59 +0300

    made list_len recursive

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,6 @@
 #include "lists.h"
 
 /**
-* get_next_node - print content of subsequent nodes
-* @node: input pointer
-* @i: input of type size_t, that tracks the number of nodes
-* Return: unsigned int value of number of nodes
-*/
-size_t get_next_node(list_t *node, size_t i)
-{
-	if (node == NULL)
-		return (i);
-	while (node->next)
-		get_next_node(node->next, i + 1);
-	return (i);
-}
-
-/**
 * list_len - function that returns the number of elements in a linked list
 * @h: list node input
 * Return: number of elements/nodes
@@ -23,12 +8,13 @@ size_t get_next_node(list_t *node, size_t i)
 
 size_t list_len(const list_t *h)
 {
-	size_t num_nodes;
+	size_t num_nodes = 1;
 
 	if (h == NULL)
 		return (0);
-
-	num_nodes = 1;
-	return (num_nodes + get_next_node(h->next, num_nodes));
+	
+	if (h->next == NULL)
+		return (num_nodes);
+	return (num_nodes += list_len(h->next));
 }
 
