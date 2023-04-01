2a831bbc8dcb26f3df275e47ff7d44bbb387d8b0
Innocent_Bern <innocentbern22@gmail.com>
2023-04-01 04:12:51 +0300

    made print_list recursive

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,30 +2,6 @@
 #include <stdio.h>
 
 /**
-* get_next_node - print content of subsequent nodes
-* @node: input pointer
-* @i: input of type size_t, that tracks the number of nodes
-* Return: unsigned int value of number of nodes
-*/
-
-size_t get_next_node(list_t *node, size_t i)
-{
-	if (node == NULL)
-		return (i);
-	if (node->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	} else
-	{
-		printf("[%d] %s\n", node->len, node->str);
-	}
-	while (node->next)
-		get_next_node(node->next, i + 1);
-
-	return (i);
-}
-
-/**
 * print_list - function that prints all the elements of a list_t
 * @h: input pointer of type list_t
 * Return: unsigned int value of number of nodes
@@ -45,6 +21,6 @@ size_t print_list(const list_t *h)
 	}
 	if (h->next == NULL)
 		return (list_len);
-	return (list_len + get_next_node(h->next, list_len));
+	return (list_len += print_list(h->next));
 }
 
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -18,3 +18,4 @@ void free_list(list_t *head)
 	free_list(head->next);
 	free(head);
 }
+
