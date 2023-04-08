4e5091e7c7e01788be0c921ca21b2c34f231173d
Innocent_Bern <innocentbern22@gmail.com>
2023-04-08 21:50:09 +0300

    Done task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* clear_bit - function that clears bits at a given index
+* @n: Address of the int we want to clear bits from
+* @index: position of the bit we want to clear.
+* Return: 1 if it worked or -1 on error
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n & ~(1u << index);
+	return (1);
+}
+
