5335edc4db231f7b126ad1dbc8989a082ff746b8
Innocent_Bern <innocentbern22@gmail.com>
2023-07-09 12:20:02 +0300

    Task 3 changes

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,4 +1,4 @@
-#include "hash_table.h"
+#include "hash_tables.h"
 
 /**
  * hash_table_get - function that gets value associated with a key
@@ -9,4 +9,19 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	size_t index;
+	hash_node_t node;
+
+	if (key == NULL || ht == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
 }
