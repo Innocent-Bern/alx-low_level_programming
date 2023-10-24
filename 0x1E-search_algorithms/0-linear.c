16252006295a648ef48d1d8c67dfd3e4e097da60
Innocent_Bern <innocentbern22@gmail.com>
2023-10-24 18:40:13 +0300

    task-0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,8 +12,12 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t index = 0;
 
+	if (array == NULL)
+		return (-1);
+
 	while (index < size)
 	{
+		printf("Value checked array[%lu] = [%d]\n", index, *array);
 		if (*array == value)
 			return (index);
 		array++;
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,7 @@
 #define _SEARCH_ALGOS_H_
 
 #include <stddef.h>
+#include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
 #endif
