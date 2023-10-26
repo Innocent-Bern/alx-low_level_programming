1f0b291e525390f6dc1eef0ccb77caa05ea793c2
Innocent_Bern <innocentbern22@gmail.com>
2023-10-26 12:00:21 +0300

    task 8

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+#include "1-binary.c"
+
+/**
+  * exponentail_search - searches for value in sorted array
+  * @array: pointer to integer array
+  * @size: number of items in the array
+  * @value: int to look up in array
+  * Return: first index of value or -1 if not found.
+*/
+
+int exponential_search(int *array, size_t size, int value)
+{
+	int bound = 1;
+
+	if (array == NULL)
+		return (-1);
+	while (bound < size && array[bound] < value)
+		bound *= 2;
+	return (binary_search(array, bound / 2 ,value));
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,10 +3,13 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <math.h>
+#define min
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 void print_func(int *array, int upper, int lower);
+int exponential_search(int *array, size_t size, int value);
 #endif
